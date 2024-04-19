7b3d702d54094ab65493f669c461ffedbcdf8f9e
subinraj021 <149981744+subinraj021@users.noreply.github.com>
2024-04-19 16:42:11 +1200

    Add files via upload

---
+++ practise/square.py
@@ -0,0 +1,14 @@
+# Square.py
+#
+# author: A. N. Other
+# date: September 2016
+ 
+side_j = int(input("Please enter the length of one side of the square\n\n"))
+print("\nThe area of your square is ", side_j ** 2,"\n\n")
+ 
+# Testing
+'''
+print("My assertions are:"
+      "\nj = 5 output = 25"
+      "\nj = 12 output = 144")
+''' 
